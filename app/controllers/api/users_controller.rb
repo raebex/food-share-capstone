6874b3a08261d41db1db0d571be826635e0fbd98
rebecca <r.young1025@gmail.com>
2021-02-26 15:31:37 -0600

    add destroy route, wrap update and destroy in current user auth

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,6 @@
 class Api::UsersController < ApplicationController
+  before_action :authenticate_user, except: [:index, :show, :create]
+
   def index
     @users = User.where(chef: true).joins(:dishes).distinct
     render "index.json.jb"
@@ -10,7 +12,7 @@ class Api::UsersController < ApplicationController
     if @user.chef || @user == current_user
       render "show.json.jb"
     else
-      render json: { message: "You're not allowed to see this page" }, status: :unauthorized
+      render json: {}, status: :unauthorized
     end
   end
 
@@ -34,4 +36,41 @@ class Api::UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find_by(id: params[:id])
+
+    if @user == current_user
+      @user.first_name = params[:first_name] || @user.first_name
+      @user.last_name = params[:last_name] || @user.last_name
+      @user.email = params[:email] || @user.email
+      @user.image_url = params[:image_url] || @user.image_url
+      # @user.password = params[:password] || @user.password
+      # @user.password_confirmation = params[:password_confirmation] || @user.password_confirmation
+      @user.chef = params[:chef] || @user.chef
+      @user.phone = params[:phone] || @user.phone
+      @user.address = params[:address] || @user.address
+      @user.bio = params[:bio] || @user.bio
+
+      if @user.save
+        render "show.json.jb"
+      else
+        render json: {errors: @user.errors.full_messages}, status: :unprocessable_entity
+      end
+    else
+      render json: {}, status: :unauthorized
+    end
+  end
+
+  def destroy
+    if @user == current_user
+      user = User.find(params[:id])
+      user.destroy
+
+      render json: {
+        message: "Successfully deleted user!"
+      }
+    else
+      render json: {}, status: :unauthorized
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
     get "/users" => "users#index"
     get "/users/:id" => "users#show"
     post "/users" => "users#create"
+    patch "/users/:id" => "users#update"
+    delete "/users/:id" => "users#destroy"
   end
 end
