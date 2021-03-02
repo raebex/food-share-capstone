c71a48ca6dce80a1be46aa3181604f5a8f574308
rebecca <r.young1025@gmail.com>
2021-03-02 15:38:48 -0600

    add user dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   
   has_many :orders
   has_many :carted_dishes
-  has_many :dishes
+  has_many :dishes, dependent: :destroy
   has_many :preorder_hours
   has_many :tickets, class_name: "Order", foreign_key: "chef_id"
   has_many :user_cuisines
