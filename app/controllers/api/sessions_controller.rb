f119167c9737ce11356da1e24e13fa0e6867f327
rebecca <r.young1025@gmail.com>
2021-03-06 21:30:41 -0600

    add chef to sessions response

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -10,7 +10,12 @@ class Api::SessionsController < ApplicationController
         Rails.application.credentials.fetch(:secret_key_base), # the secret key
         "HS256" # the encryption algorithm
       )
-      render json: { jwt: jwt, email: user.email, user_id: user.id }, status: :created
+      render json: {
+        jwt: jwt,
+        email: user.email,
+        user_id: user.id,
+        chef: user.chef
+      }, status: :created
     else
       render json: {}, status: :unauthorized
     end
