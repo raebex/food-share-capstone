fdb0bbec0e3d18d7d95f08d619c962a2e2206a25
rebecca <r.young1025@gmail.com>
2021-03-31 18:38:52 -0500

    whitelist frontend app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080'
+    origins 'localhost:8080', 'distracted-beaver-3fe522.netlify.app'
 
     resource '*',
       headers: :any,
